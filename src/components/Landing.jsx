fa0bbfd5377afc0c9e624b7255fb88553a1bad61
Nathan Jones <nathan@jonesco.autos>
2023-03-23 12:55:40 -0500

    d

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,136 +3,103 @@ import { useState } from 'react'
 import { CheckIcon } from '@heroicons/react/20/solid'
 import racinglogo from '../images/logos/racing.png'
 
-const frequencies = [
-  { value: 'monthly', label: 'Monthly', priceSuffix: '/month' },
-  { value: 'annually', label: 'Annually', priceSuffix: '/year' },
-]
-const tiers = [
+const category = [
   {
     name: 'Racing',
-    id: 'tier-freelancer',
-    href: 'https://racing.jonesco.autos',
-    description: 'The essentials to provide your best work for clients.',
-    features: [
-      '5 products',
-      'Up to 1,000 subscribers',
-      'Basic analytics',
-      '48-hour support response time',
-    ],
-    mostPopular: false,
-    logo: { racinglogo },
+    link: 'https://racing.jonesco.autos',
+    role: 'Co-Founder / CEO',
+    imageUrl: '/../images/logos/racing.png',
   },
   {
     name: 'Off-Road',
-    id: 'tier-startup',
-    href: 'https://offroad.jonesco.autos',
-    description: 'A plan that scales with your rapidly growing business.',
-    features: [
-      '25 products',
-      'Up to 10,000 subscribers',
-      'Advanced analytics',
-      '24-hour support response time',
-      'Marketing automations',
-    ],
-    mostPopular: false,
-    logo: { racinglogo },
+    link: 'https://offroad.jonesco.autos',
+    role: 'Co-Founder / CEO',
+    imageUrl: '/../images/logos/offroad.png',
   },
   {
     name: 'Trucks',
-    id: 'tier-enterprise',
-    href: 'https://trucks.jonesco.autos',
-    description: 'Dedicated support and infrastructure for your company.',
-    features: [
-      'Unlimited products',
-      'Unlimited subscribers',
-      'Advanced analytics',
-      '1-hour, dedicated support response time',
-      'Marketing automations',
-      'Custom reporting tools',
-    ],
-    mostPopular: false,
-    logo: { racinglogo },
+    link: 'https://trucks.jonesco.autos',
+    role: 'Co-Founder / CEO',
+    imageUrl: '/../images/logos/trucks.png',
   },
 ]
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+const social = [
+  {
+    name: 'Facebook',
+    link: 'https://www.facebook.com/jonesco.autos',
+    imageUrl: '/../images/logos/facebook.svg',
+  },
+  {
+    name: 'YouTube',
+    link: 'https://www.youtube.com/@JonesCoAutos',
+    imageUrl: '/../images/logos/youtube.svg',
+  },
+  {
+    name: 'Twitter',
+    link: 'https://twitter.com/JonesCoAutos',
+    imageUrl: '/../images/logos/twitter.svg',
+  },
+  {
+    name: 'LinkedIn',
+    link: 'https://trucks.jonesco.autos',
+    imageUrl: '/../images/logos/linkedin.svg',
+  },
+  {
+    name: 'Instagram',
+    link: 'https://www.instagram.com/jonesco.autos/',
+    imageUrl: '/../images/logos/instagram.svg',
+  },
+  {
+    name: 'Pinterest',
+    link: 'https://www.pinterest.com/jonescoautos/',
+    imageUrl: '/../images/logos/pinterest.svg',
+  },
+  {
+    name: 'TikTok',
+    link: 'https://www.tiktok.com/@jonescoautos',
+    imageUrl: '/../images/logos/facebook.svg',
+  },
+]
 
 export default function Example() {
-  const [frequency, setFrequency] = useState(frequencies[0])
-
   return (
-    <div className="h-screen bg-gradient-to-r from-red-500 via-green-500 to-blue-500">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-4xl text-center">
-          <p className="pt-24 font-fugaz text-4xl font-bold tracking-tight text-white sm:text-7xl ">
-            JONES CO.
-          </p>
-        </div>
-        <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-300">
-          Turn your vision into reality.
-        </p>
-        <div className="mt-16 flex justify-center"></div>
-        <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {tiers.map((tier) => (
-            <div
-              key={tier.id}
-              className={classNames(
-                tier.mostPopular
-                  ? 'bg-white/5 ring-2 ring-indigo-500'
-                  : 'ring-2 ring-white',
-                'rounded-3xl p-8 xl:p-10'
-              )}
-            >
-              <div>
-                <Image src={tier.logo} alt="Logo" className="h-10 w-auto" />
+    <div className="grid grid-cols-1 place-content-center gap-6 p-10 sm:h-screen">
+      <div className="">
+        <h1 className="text-center font-fugaz text-3xl sm:text-6xl">
+          JONES CO.
+        </h1>
+        <h2 className="-mt-1 text-center font-fugaz text-xl sm:text-3xl">
+          AUTOMOTIVE
+        </h2>
+      </div>
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 sm:px-48">
+        {category.map((category) => (
+          <div
+            key={category.link}
+            className="relative flex place-content-center items-center space-x-3 rounded-lg border border-gray-300 bg-gray-100 p-2 shadow-sm focus-within:ring-2 focus-within:ring-lime-500 focus-within:ring-offset-2 hover:border-gray-400"
+          >
+            <a href={category.link}>
+              <div className="flex-shrink-0">
+                <img className="h-auto w-48" src={category.imageUrl} alt="" />
               </div>
-              <div className="flex items-center justify-between gap-x-4">
-                <h3
-                  id={tier.id}
-                  className="text-lg font-semibold leading-8 text-white"
-                >
-                  {tier.name}
-                </h3>
-                {tier.mostPopular ? (
-                  <p className="rounded-full bg-indigo-500 py-1 px-2.5 text-xs font-semibold leading-5 text-white">
-                    Most popular
-                  </p>
-                ) : null}
+            </a>
+          </div>
+        ))}
+      </div>
+      <div className="grid grid-cols-3 place-content-center items-center gap-4 sm:grid-cols-7 sm:px-48">
+        {social.map((social) => (
+          <div
+            key={social.link}
+            className="relative flex place-content-center items-center space-x-3 rounded-lg border border-gray-300 bg-gray-300 p-4 shadow-sm focus-within:ring-2 focus-within:ring-lime-500 focus-within:ring-offset-2 hover:border-gray-400 sm:px-6 sm:py-5"
+          >
+            <a href={social.link}>
+              <div className="flex-shrink-0">
+                <img className="h-auto w-full" src={social.imageUrl} alt="" />
               </div>
-              <p className="mt-4 text-sm leading-6 text-gray-300">
-                {tier.description}
-              </p>
-              <a
-                href={tier.href}
-                aria-describedby={tier.id}
-                className={classNames(
-                  tier.mostPopular
-                    ? 'bg-indigo-500 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline-indigo-500'
-                    : 'bg-white/10 text-white hover:bg-white/20 focus-visible:outline-white',
-                  'mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2'
-                )}
-              >
-                Visit Site{' '}
-              </a>
-              <ul
-                role="list"
-                className="mt-8 space-y-3 text-sm leading-6 text-gray-300 xl:mt-10"
-              >
-                {tier.features.map((feature) => (
-                  <li key={feature} className="flex gap-x-3">
-                    <CheckIcon
-                      className="h-6 w-5 flex-none text-white"
-                      aria-hidden="true"
-                    />
-                    {feature}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
+            </a>
+          </div>
+        ))}
       </div>
     </div>
   )
