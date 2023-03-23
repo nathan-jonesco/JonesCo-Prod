88af653f5d9e3809aa827cd7a7da221fd04c4d27
Nathan Jones <nathan@jonesco.autos>
2023-03-23 13:13:03 -0500

    g

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,7 +1,5 @@
 import Image from 'next/image'
 import { useState } from 'react'
-import { CheckIcon } from '@heroicons/react/20/solid'
-import racinglogo from '../images/logos/racing.png'
 
 const category = [
   {
@@ -64,42 +62,50 @@ const social = [
 
 export default function Example() {
   return (
-    <div className="grid grid-cols-1 place-content-center gap-6 p-10 sm:h-screen">
-      <div className="">
-        <h1 className="text-center font-fugaz text-3xl sm:text-6xl">
-          JONES CO.
-        </h1>
-        <h2 className="-mt-1 text-center font-fugaz text-xl sm:text-3xl">
-          AUTOMOTIVE
-        </h2>
-      </div>
-      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 sm:px-48">
-        {category.map((category) => (
-          <div
-            key={category.link}
-            className="relative flex place-content-center items-center space-x-3 rounded-lg border border-gray-300 bg-gray-100 p-2 shadow-sm focus-within:ring-2 focus-within:ring-lime-500 focus-within:ring-offset-2 hover:border-gray-400"
-          >
-            <a href={category.link}>
-              <div className="flex-shrink-0">
-                <img className="h-auto w-48" src={category.imageUrl} alt="" />
-              </div>
-            </a>
-          </div>
-        ))}
-      </div>
-      <div className="grid grid-cols-3 place-content-center items-center gap-4 sm:grid-cols-7 sm:px-48">
-        {social.map((social) => (
-          <div
-            key={social.link}
-            className="relative flex place-content-center items-center space-x-3 rounded-lg border border-gray-300 bg-gray-300 p-4 shadow-sm focus-within:ring-2 focus-within:ring-lime-500 focus-within:ring-offset-2 hover:border-gray-400 sm:px-6 sm:py-5"
-          >
-            <a href={social.link}>
-              <div className="flex-shrink-0">
-                <img className="h-auto w-full" src={social.imageUrl} alt="" />
-              </div>
-            </a>
-          </div>
-        ))}
+    <div className="">
+      <video autoPlay loop muted
+        src='/../videos/drift.mp4'
+        className="absolute -z-10 min-h-full w-auto min-w-full max-w-none"
+      ></video>
+             <div className="absolute -z-10 min-h-full w-auto min-w-full max-w-none bg-gray-900 opacity-80"></div>
+
+      <div className="grid grid-cols-1 place-content-center gap-6 sm:h-screen z-auto">
+        <div className="">
+          <h1 className="text-white text-center font-fugaz text-3xl sm:text-6xl">
+            JONES CO.
+          </h1>
+          <h2 className="text-gray-300 -mt-1 text-center font-fugaz text-xl sm:text-3xl">
+            AUTOMOTIVE
+          </h2>
+        </div>
+        <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 sm:px-48">
+          {category.map((category) => (
+            <div
+              key={category.link}
+              className="relative flex place-content-center items-center space-x-3 rounded-lg border border-gray-300 bg-gray-100 p-2 shadow-sm focus-within:ring-2 focus-within:ring-lime-500 focus-within:ring-offset-2 hover:border-gray-400"
+            >
+              <a href={category.link}>
+                <div className="flex-shrink-0">
+                  <img className="h-auto w-48" src={category.imageUrl} alt="" />
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
+        <div className="grid grid-cols-3 place-content-center items-center gap-4 sm:grid-cols-7 sm:px-48">
+          {social.map((social) => (
+            <div
+              key={social.link}
+              className="relative flex place-content-center items-center space-x-3 rounded-lg border border-gray-300 bg-gray-300 p-4 shadow-sm focus-within:ring-2 focus-within:ring-lime-500 focus-within:ring-offset-2 hover:border-gray-400 sm:px-6 sm:py-5"
+            >
+              <a href={social.link}>
+                <div className="flex-shrink-0">
+                  <img className="h-auto w-full" src={social.imageUrl} alt="" />
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
