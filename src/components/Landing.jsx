3cb40dd47792a579b84a95d7c8e6cce270999fa2
Nathan Jones <nathan@jonesco.autos>
2023-03-24 15:40:18 -0500

    j

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,16 +1,40 @@
 import Image from 'next/image'
 import { useState } from 'react'
 
+const contact = [
+  {
+    name: 'Text',
+    link: 'https://racing.jonesco.autos',
+    imageUrl: '/../images/logos/text.svg',
+    imageClass:
+      'rounded-xl border border-green-600 bg-gradient-to-br from-gray-300 to-green-500 p-2 shadow-sm focus-within:ring-2 focus-within:ring-green-500 focus-within:ring-offset-2',
+  },
+  {
+    name: 'Email',
+    link: 'https://offroad.jonesco.autos',
+    imageUrl: '/../images/logos/offroad.png',
+    imageClass:
+      'relative flex place-content-center items-center space-x-3 rounded-3xl border border-offroad-yellow-600 bg-gradient-to-br from-gray-300 to-offroad-yellow-500 p-2 shadow-sm focus-within:ring-2 focus-within:ring-offroad-yellow-500 focus-within:ring-offset-2',
+  },
+  {
+    name: 'Call',
+    link: 'https://trucks.jonesco.autos',
+    imageUrl: '/../images/logos/trucks.png',
+    imageClass:
+      'relative flex place-content-center items-center space-x-3 rounded-3xl border border-trucks-blue-600 bg-gradient-to-br from-gray-300 to-trucks-blue-800 p-2 shadow-sm focus-within:ring-2 focus-within:ring-trucks-blue-500 focus-within:ring-offset-2',
+  },
+]
+
 const category = [
   {
-    name: 'Racing',
+    name: 'Call',
     link: 'https://racing.jonesco.autos',
     imageUrl: '/../images/logos/racing.png',
     imageClass:
       'relative flex place-content-center items-center space-x-3 rounded-3xl border border-racing-red-600 bg-gradient-to-br from-gray-300 to-racing-red-500 p-2 shadow-sm focus-within:ring-2 focus-within:ring-racing-red-500 focus-within:ring-offset-2',
   },
   {
-    name: 'Off-Road',
+    name: 'Text',
     link: 'https://offroad.jonesco.autos',
     imageUrl: '/../images/logos/offroad.png',
     imageClass:
@@ -80,11 +104,14 @@ export default function Example() {
           className="absolute -z-10 h-full w-auto min-w-full object-cover"
         ></video>
       </div>
+      
+
+      
       <div className="max-w-none place-content-center items-center">
         <div className="z-auto grid grid-cols-1 place-content-center gap-6 sm:h-screen  ">
           <div className="absolute -z-10 min-h-full  min-w-full max-w-none bg-gradient-to-t from-gray-700 to-gray-900 opacity-90"></div>
           <div className="">
-            <h1 className="pt-14 text-center font-fugaz text-4xl text-gray-50 sm:pt-8 sm:text-6xl md:text-9xl">
+            <h1 className="pt-32 text-center font-fugaz text-5xl text-gray-50 sm:pt-8 sm:text-6xl md:text-9xl">
               JONES CO.
             </h1>
             <h2 className="-mt-1 text-center font-fugaz text-xl text-gray-300 sm:text-3xl md:text-5xl"></h2>
@@ -92,13 +119,14 @@ export default function Example() {
               Your vision, is our passion. 💯
             </p>
           </div>
+        
           <div className="grid grid-cols-3 gap-2 px-2 sm:px-16  md:gap-4 md:px-32 lg:px-52 xl:px-80">
             {category.map((category) => (
               <div key={category.link} className={category.imageClass}>
                 <a href={category.link}>
                   <div className="flex-shrink-0">
                     <img
-                      className="h-auto w-40 md:py-5"
+                      className="h-auto w-40 lg:w-64 md:py-5"
                       src={category.imageUrl}
                       alt=""
                     />
@@ -111,12 +139,12 @@ export default function Example() {
             {social.map((social) => (
               <div
                 key={social.link}
-                className="md:h-18 focus-within:ring-offset-6 lg:h-18 relative flex h-16 place-content-center items-center space-x-3 rounded-2xl border border-gray-200 bg-gradient-to-bl from-gray-100 to-gray-400 opacity-90 shadow-lg focus-within:ring-2"
+                className="lg:h-20 focus-within:ring-offset-6 relative flex place-content-center items-center space-x-3 rounded-2xl border border-gray-200 bg-gradient-to-bl from-gray-100 to-gray-400 opacity-90 shadow-lg focus-within:ring-2"
               >
                 <a href={social.link}>
                   <div className="flex-shrink-0">
                     <img
-                      className="w-auto py-2 md:h-14 lg:h-16"
+                      className="w-auto py-2 h-16 md:h-18 lg:h-18"
                       src={social.imageUrl}
                       alt=""
                     />
