206f50f0118b22445775bc2342bfb03874f7aee6
Nathan Jones <nathan@jonesco.autos>
2023-04-11 16:05:28 -0500

    l

--- src/components/CTA-B.jsx
+++ src/components/CTA-B.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Example() {
   return (
     <div className="bg-JC-gold-100">
@@ -8,18 +10,18 @@ export default function Example() {
           <span className="text-5xl font-jc tracking-tight text-JC-gold-900 sm:text-8xl">We Can Fix That.</span>
         </h2>
         <div className="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
-          <a
+          <Link
             href="/contact"
             className="rounded-md bg-JC-gold-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-JC-gold-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-JC-gold-600"
           >
             Get started
-          </a>
-          <a
+          </Link>
+          <Link
             href="/services"
             className="text-sm font-semibold leading-6 text-JC-gold-900"
           >
             Services <span aria-hidden="true">→</span>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
