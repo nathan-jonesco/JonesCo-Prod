07357460376afbb11b4f139189e4a47d02c0ab8f
Nathan Jones <nathan@jonesco.autos>
2023-04-11 14:55:26 -0500

    l

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,13 +1,13 @@
 export default function Example() {
   return (
     <>
-      <div className="flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8 bg-JC-gold-200">
+      <div className="flex flex-col justify-center py-6 sm:py-12 bg-JC-gold-200">
       <iframe
               src="https://docs.google.com/forms/d/e/1FAIpQLSeBM_cWE7XlGNsVKaFj-Xdgy4fWzU1dFAl5KDLUX3Eztv9kow/viewform?embedded=true"
               frameborder="0"
               marginheight="0"
               marginwidth="0"
-              height='1900'
+              height='2000'
               className="w-full"
             >
               Loading…
